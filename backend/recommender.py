2b466a2f6d773a6cad8e3159cec6961397961603
bedivanshbedi-design <bedi.vanshbedi@gmail.com>
2026-05-12 15:54:42 +0000

    ll

--- backend/recommender.py
+++ backend/recommender.py
@@ -1,7 +1,8 @@
 import sys
 import os
 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+sys.path.insert(0, ROOT_DIR)
 from config.mappings import FACE_SHAPE_MAP
 
 def recommend_glasses(shape):
