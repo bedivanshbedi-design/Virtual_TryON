6f0839dea48064c08d4f9a8fa16239aae6aa3f4a
bedivanshbedi-design <bedi.vanshbedi@gmail.com>
2026-05-13 11:56:50 +0000

    new

--- app/face_shape.py
+++ app/face_shape.py
@@ -4,19 +4,19 @@ def classify_face_shape(landmarks):
 
     (x, y, w, h) = landmarks[0]
 
-    face_width = w
-    face_height = h
+    face_width = w*1.1
+    face_height = h*0.9
 
     if face_width == 0:
         return "unknown"
 
     ratio = face_height/face_width
 
-    if ratio > 1.5:
+    if ratio > 1.35:
         return "Oval"
-    elif 1.2 < ratio <= 1.5:
+    elif 1.15 < ratio <= 1.35:
         return "Heart"
-    elif 0.9 < ratio < 1.2:
+    elif 0.95 < ratio < 1.15:
         return "Round"
     else:
         return "Square"
