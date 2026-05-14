d3e988332b48181bf8bd76efe23f396cedb6249d
bedivanshbedi-design <bedi.vanshbedi@gmail.com>
2026-05-14 05:33:48 +0000

    new

--- app/face_detection.py
+++ app/face_detection.py
@@ -7,7 +7,7 @@ mp_face_mesh = mp.solutions.face_mesh
 
 def get_landmarks(image):
     with mp_face_mesh.FaceMesh(static_image_mode=True) as face_mesh:
-        rgb = cv2.cvtCOlor(image, cv2.COLOR_BGR2BGR)
+        rgb = cv2.cvtColor(image, cv2.COLOR_BGR2BGR)
         results = face_mesh.process(rgb)
 
         if not results.multi_face_landmarks:
